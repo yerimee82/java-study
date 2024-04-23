c7d0b288d3aa3824c436870a6a86be876659f7db
Kim, Yerim <yerim343@naver.com>
2024-04-23 15:24:55 +0900

    prob3 solved.

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -9,7 +9,20 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		int inputNum;
+		int result = 0;
 		
+		System.out.println("숫자를 입력하세요: ");
+		inputNum = scanner.nextInt();
+		
+		for(int i=0; i<inputNum+1; i++) {
+			if ( inputNum % 2 == 0 && i % 2 == 0) { // 짝수 
+				result += i;
+			} else if(inputNum % 2 != 0 && i % 2 == 1) { // 홀
+				result += i;
+			}
+		}
+		System.out.println("결과 값 : " + result);
 		scanner.close();
 	}
 }
