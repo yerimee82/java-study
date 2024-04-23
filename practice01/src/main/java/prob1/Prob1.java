4cf06b13e2aac5bc8afc4426b32062bdade046ce
Kim, Yerim <yerim343@naver.com>
2024-04-23 15:00:01 +0900

    prob1 solved.

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,6 +7,15 @@ public class Prob1 {
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
+		int num;
+		System.out.println("수를 입력하시오 : ");
+		num = scanner.nextInt();
+		
+		if (num % 3 == 0) {
+			System.out.println("3의 배수 입니다.");
+		} else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
 		
 		scanner.close();
 	}
