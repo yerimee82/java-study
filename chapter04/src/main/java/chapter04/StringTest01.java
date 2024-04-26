30d2c4cfc0eb9e7d8dbd403d98d3acfe328bbafc
Kim, Yerim <yerim343@naver.com>
2024-04-26 14:52:23 +0900

    .gitignore 수정

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,11 @@
+package chapter04;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        // c:\temp
+        System.out.println("c:\\temp");
+
+        // "hello"
+        System.out.println("\"hello\"");
+    }
+}
