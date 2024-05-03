db676ad41680e012ea8dca0cb70b9cb6751c80b5
Kim, Yerim <yerim343@naver.com>
2024-05-03 10:41:53 +0900

    TCP Server fix.

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -17,52 +17,65 @@ public class TCPServer {
             // 1.1 FIN-WAIT2 -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해
             serverSocket.setReuseAddress(true);
 
-
             // 2. 바인딩(binding)
             //  socket에 InetSocketAddress[InetAddress(IPAddress + Port)] 를 바인딩 한다.
             //  IPAddress: 0.0.0.0 : 특정 호스트 IP 를 바인딩 하지 않는다. -> 모든 가능한 IP 주소 허용
             serverSocket.bind(new InetSocketAddress("0.0.0.0", 5050));
 
             // 3. accept
-            socket = serverSocket.accept();    // blocking
+            socket = serverSocket.accept();  // blocking
 
-            InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-            String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-            int remotePort = inetRemoteSocketAddress.getPort();
-            System.out.println("[server] conneted by client [" + remoteHostAddress + ":" + remotePort + "]");
+            try {
+                InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+                String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+                int remotePort = inetRemoteSocketAddress.getPort();
+                System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
 
-            // 4. IO Stream 받아오기
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
+                //4. IO Stream 받아오기
+                InputStream is = socket.getInputStream();
+                OutputStream os = socket.getOutputStream();
 
-            while (true) {
-                System.out.println("try to read");
-                // 5. 데이터 읽기
-                byte[] buffer = new byte[256];
-                int readByteCount = is.read(buffer);    // blocking
-                if (readByteCount == -1) {
-                    // Connection Reset -- closed by client
-                    System.out.println("[server] closed by client");
-                    break;
+                while (true) {
+                    // System.out.println("try to read");
+                    //5. 데이터 읽기
+                    byte[] buffer = new byte[256];
+                    int readByteCount = is.read(buffer); // blocking
+                    if (readByteCount == -1) {
+                        // closed by client
+                        System.out.println("[server] closed by client");
+                        break;
+                    }
+
+                    String data = new String(buffer, 0, readByteCount, "UTF-8");
+                    System.out.println("[server] received:" + data);
+
+                    // 6. 데이터 쓰기
+                    // SO_TIMEOUT 테스트
+//                    os.write(data.getBytes("utf-8"));
+//
+//					try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+
+                    os.write(data.getBytes("utf-8"));
                 }
-
-                String data = new String(buffer, 0, readByteCount, StandardCharsets.UTF_8);
-                System.out.println("[server] received:" + data);
-
-                // 6. 데이터 쓰기
-//                os.write(data.getBytes(StandardCharsets.UTF_8));
-
+            } catch (SocketException e) {
+                System.out.println("[server] Socket Exception: " + e);
+            } catch (IOException e) {
+                System.out.println("[server] error:" + e);
+            } finally {
                 try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
+                    if (socket != null && !socket.isClosed()) {
+                        socket.close();
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                os.write(data.getBytes("utf-8"));
             }
 
-        }
-        catch (SocketException e) {
+        } catch (SocketException e) {
             System.out.println("[server] SocketException :" + e);
         } catch (IOException e) {
             System.out.println("[server] error: " + e);
